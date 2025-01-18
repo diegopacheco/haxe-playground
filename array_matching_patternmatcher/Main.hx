79cec90e283f01c7a4c423392004937c743aeeb1
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-18 00:09:24 -0800

    Haxe array_matching_patternmatcher

---
+++ array_matching_patternmatcher/Main.hx
@@ -0,0 +1,14 @@
+class Main {
+    static public function main() {
+        var myArray = [1, 6];
+
+        var match = switch (myArray) {
+            case [2, _]: "0";
+            case [_, 6]: "1";
+            case []: "2";
+            case [_, _, _]: "3";
+            case _: "4";
+        }
+        trace(match); // 1
+    }
+}
