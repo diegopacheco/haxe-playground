c44575cb45b04dcedfaf80a77dc1965b31d899d1
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-18 00:09:24 -0800

    Haxe or_patterns_patternmatcher

---
+++ or_patterns_patternmatcher/Main.hx
@@ -0,0 +1,10 @@
+class Main {
+    static public function main() {
+        var match = switch (7) {
+            case 4 | 1: "0";
+            case 6 | 7: "1";
+            case _: "2";
+          }
+        trace(match); // 1
+    }
+}
