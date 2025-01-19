4188d2c79bedd13edf3341ff3b7db6811afaccf6
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-19 00:23:36 -0800

    Haxe guards_pattern_matcher

---
+++ guards_pattern_matcher/Main.hx
@@ -0,0 +1,13 @@
+class Main {
+    static public function main() {
+        var myArray = [7, 6];
+        var s = switch (myArray) {
+            case [a, b] if (b > a):
+                b + ">" + a;
+            case [a, b]:
+                b + "<=" + a;
+            case _: "found something else";
+        }
+        trace(s); // 6<=7
+    }
+}
