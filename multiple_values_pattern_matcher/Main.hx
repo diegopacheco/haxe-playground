890ab910bc3ec3da1afca95949b244e017718c3d
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-19 00:23:37 -0800

    Haxe multiple_values_pattern_matcher

---
+++ multiple_values_pattern_matcher/Main.hx
@@ -0,0 +1,10 @@
+class Main {
+    static public function main() {
+        var s = switch [1, false, "foo"] {
+            case [1, false, "bar"]: "0";
+            case [_, true, _]: "1";
+            case [_, false, _]: "2";
+          }
+        trace(s); // 2
+    }
+}
