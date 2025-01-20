62d6f5f327de0912fdf1dce89b7e64cab89705e5
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-20 00:11:01 -0800

    Haxe null_type

---
+++ null_type/Main.hx
@@ -0,0 +1,7 @@
+class Main {
+    static public function main() {
+        var n : Null<Int> = null;
+        var a : Int = n;
+        trace(a); // 0 on static platforms
+    }
+}
---
+++ null_type/README.md
@@ -0,0 +1,12 @@
+### Result
+
+```
+❯ ./run.sh
+Main.hx:5: null
+❯ ./run-java.sh
+Main.hx:5: 0
+❯ ./run-nodejs.sh
+Main.hx:5: null
+❯ ./run-python.sh
+None
+```
